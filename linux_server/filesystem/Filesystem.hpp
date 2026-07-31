c746d5a67567b2bceb81894a1c275ac3093998ea
Claus Prüfer <pruefer@webcodex.de>
2026-07-31 15:38:50 +0200

    Update

--- linux_server/filesystem/Filesystem.hpp
+++ linux_server/filesystem/Filesystem.hpp
@@ -12,18 +12,8 @@ struct ServerFile {
     unsigned int ContentLength;
 };
 
-#include "../../src/components/filesystem/filedata.h"
-#define ServerFiles BaseServerFiles
-#include "../../src/components/filesystem/filemetadata.h"
-#undef ServerFiles
-
-#if __has_include("filedata-test.h") && __has_include("filemetadata-test.h")
 #include "filedata-test.h"
 #include "filemetadata-test.h"
-#define LINUX_SERVER_HAS_TEST_SERVER_FILES 1
-#else
-#define LINUX_SERVER_HAS_TEST_SERVER_FILES 0
-#endif
 
 class Filesystem
 {
@@ -34,11 +24,7 @@ public:
 
     static ServerFile getFileMetadata(string URLPath)
     {
-#if LINUX_SERVER_HAS_TEST_SERVER_FILES
         for(const ServerFile &FileMetadata: ServerFiles) {
-#else
-        for(const ServerFile &FileMetadata: BaseServerFiles) {
-#endif
             if (FileMetadata.ContentPath == URLPath) {
                 return FileMetadata;
             }
